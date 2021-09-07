03afc0fdc5add3c8d35fb648d80a7e8adbc5f5d9
CrystalManMan <86424080+CrystalManMan@users.noreply.github.com>
2021-09-06 20:13:12 -0700

    Update updatedcrash.lua

--- updatedcrash.lua
+++ updatedcrash.lua
@@ -1,1 +1,7 @@
 setclipboard('https://discord.gg/43jPGPvsjj')
+game.StarterGui:SetCore("SendNotification", {
+		Title = "Crystal Central's Crash";
+		Text = "3 minute server crasher discord copied to clipbord"; 
+		Icon = "rbxassetid://0"; 
+		Duration = 10; 
+	})
