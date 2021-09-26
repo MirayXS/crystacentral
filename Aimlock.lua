3550b97f435aa38f13435f653f9f2a5228a18a46
CrystalManMan <86424080+CrystalManMan@users.noreply.github.com>
2021-09-25 21:47:57 -0700

    Update Aimlock.lua

--- Aimlock.lua
+++ Aimlock.lua
@@ -1,2 +1,3 @@
-
+setclipboard("https://discord.gg/CeG9FkX5")
+wait(2)
 game:GetService("Player").LocalPlayer:Kick("Script is down, it is now paid and is in WIP join the discord to get it! Copied to clipbord.")
